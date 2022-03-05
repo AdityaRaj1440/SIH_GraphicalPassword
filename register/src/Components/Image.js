5aa34b53c785c9c512ddd703339c7b782a56e80e
AdityaRaj1440 <raj.aaditya1440@gmail.com>
2022-03-05 10:03:35 +0530

    Image selection feature added

--- register/src/Components/Image.js
+++ register/src/Components/Image.js
@@ -1,10 +1,29 @@
+import {useState} from 'react'
+const arr= []
 const Image = ({url}) => { 
+ 
 
+    const [list, setList]= useState([])
+    console.log(list);
+
+    const addImage = () => {
+        if(!arr.includes(url)&&arr.length!==5)
+        {
+            arr.push(url)
+            setList(arr)
+        }
+        else if(arr.includes(url))
+        {
+            arr.splice(arr.indexOf(url),1)
+            setList(arr)
+        }
+       
+    }
     
+
     return(
     <>
-    
-    <img src= {url} alt= "img1" id="1" width= "15%"/>
+    <img src= {url} alt= "img1" width= "15%" onClick={addImage}/>
           &nbsp;&nbsp;
     </>
 )
