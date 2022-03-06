28e9ed181e7ece2066430c5b58cb1bd8aaacd46b
Tushar <reddygtvs@gmail.com>
2022-03-07 01:29:49 +0530

    basic navbar layout

--- login/src/App.js
+++ login/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 import Login from './Login';
+import './App.css';
+import Navbar from './Navbar';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const App = () => {
-  return (<Login />);
+  return (
+  <Navbar />
+  // <Login />
+  );
 }
  
 export default App;
