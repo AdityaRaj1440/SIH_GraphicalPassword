98cc65a114ff204ab115a68499605e2d3708223a
Anannya Mahapatra <anannya1947@gmail.com>
2022-03-06 10:52:58 +0530

    image grid display trial

--- login/src/Login.js
+++ login/src/Login.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import React from "react";
+import ImageGrid from "./ImageGrid";
 
 const Login = () => {
     const [username, setUsername] = useState("")
@@ -13,14 +14,15 @@ const Login = () => {
         <div className="App">
             <h1>Login</h1>
             <h3>Enter your username</h3>
-            <input type="text" 
-            onChange={(e) => {
-                setUsername(e.target.value)
-            }}/>
+            <input type="text"
+                onChange={(e) => {
+                    setUsername(e.target.value)
+                }} />
+            <ImageGrid />
             <button onClick={submit}>Submit</button>
         </div>
     );
 }
- 
+
 export default Login;
 
