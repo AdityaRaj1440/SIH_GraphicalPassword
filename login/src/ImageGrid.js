69cf2cb1221bdeafbe7b39da08b0affb3665b8ac
Anannya Mahapatra <anannya1947@gmail.com>
2022-03-06 11:14:26 +0530

    5 images grid

--- login/src/ImageGrid.js
+++ login/src/ImageGrid.js
@@ -4,7 +4,13 @@ import ImageListItem from '@mui/material/ImageListItem';
 
 export default function ImageGrid() {
     return (
-        <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+        <ImageList sx={{
+            width: 500,
+            //  height: 450
+        }}
+            cols={5}
+            rowHeight={100}
+        >
             {itemData.map((item) => (
                 <ImageListItem key={item.img}>
                     <img
@@ -40,32 +46,32 @@ const itemData = [
         img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
         title: 'Hats',
     },
-    {
-        img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-        title: 'Honey',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-        title: 'Basketball',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-        title: 'Fern',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-        title: 'Mushrooms',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-        title: 'Tomato basil',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-        title: 'Sea star',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-        title: 'Bike',
-    },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+    //     title: 'Honey',
+    // },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+    //     title: 'Basketball',
+    // },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+    //     title: 'Fern',
+    // },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+    //     title: 'Mushrooms',
+    // },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+    //     title: 'Tomato basil',
+    // },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+    //     title: 'Sea star',
+    // },
+    // {
+    //     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+    //     title: 'Bike',
+    // },
 ];
